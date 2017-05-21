beda68712e5fa4eea298902d6f9fb1f3b10e3203
Lebold, Josh <LEBOLDJ@nationwide.com>
2017-05-21 18:13:50 -0400

    mostly css changes again

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`
+  templateUrl: './servers.component.html',
+  styleUrls: ['../servers/servers.component.css']
 })
 export class ServersComponent{
 
